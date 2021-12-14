88d51ac81521555385a36c79a5169dd4510ea66d
Sanae Benali <sanae.benali@gmail.com>
2021-12-14 21:33:06 +0100

    first push yelp

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+   resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:index, :create] do
+      resources :new
+    end
+   end
 end
